27c626d4d9cfce6407823709981a33e0616a2d48
Maria Scheer Norreen <mariascheernorreen@marias-mbp.home>
2019-01-05 22:25:54 +0100

    Jeg har samlet det hele fra "eksperiment  mappen"

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/////////HER STARTER BURGERMENUEN ////////////////////////////////
+
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 
 document.querySelector("#burgermenu").addEventListener("click", burgerMenu);
@@ -10,3 +12,112 @@ function burgerMenu() {
         x.className = "topnav";
     }
 }
+
+
+/////////HER STARTER SPLASH ANIMATIONER ////////////////////////////////
+
+window.addEventListener("load", showPage);
+
+/* Her skal defineres en variabel for pile op og ned som skal være true som default */
+
+function showPage() {
+    console.log("showPage");
+    bigBox();
+}
+
+
+function bigBox() {
+    console.log("bigBox");
+    document.querySelector("#box2").classList.add("in_right");
+    document.querySelector("#circle1").classList.add("in_bottom");
+    document.querySelector("#circle2").classList.add("in_bottom2");
+    document.querySelector("#circle3").classList.add("in_bottom3");
+
+}
+
+document.querySelector("#box2").addEventListener("animationend", smallBoxes);
+
+
+function smallBoxes() {
+    console.log("smallBoxes");
+    document.querySelector("#box1").classList.add("in_left_2");
+    document.querySelector("#box3").classList.add("in_left");
+}
+
+document.querySelector("#box3").addEventListener("animationend", lettersPortfolio);
+
+function lettersPortfolio() {
+    console.log("lettersPortfolio");
+
+
+    document.querySelector("#streg").classList = ("streg_class");
+    document.querySelector("#letter1").classList = ("moving_letters");
+    document.querySelector("#letter2").classList = ("moving_letters_2");
+    document.querySelector("#letter3").classList = ("moving_letters_3");
+    document.querySelector("#letter4").classList = ("moving_letters_4");
+    document.querySelector("#letter5").classList = ("moving_letters_5");
+    document.querySelector("#letter6").classList = ("moving_letters_6");
+    document.querySelector("#letter7").classList = ("moving_letters_7");
+    document.querySelector("#letter8").classList = ("moving_letters_8");
+    document.querySelector("#letter9").classList = ("moving_letters_9");
+    document.querySelector("#letter10").classList = ("moving_letters_10");
+
+
+}
+
+document.querySelector("#streg").addEventListener("animationend", hideStreg);
+
+function hideStreg() {
+    console.log("hideStreg");
+    document.querySelector("#streg").classList = ("hide");
+    nameAppear();
+
+}
+
+
+function nameAppear() {
+    console.log("nameAppear");
+    document.querySelector("#name").classList = ("appear");
+
+}
+
+document.querySelector("#name").addEventListener("animationend", buttonAppear);
+
+function buttonAppear() {
+    console.log("buttonAppear");
+    document.querySelector("#splash_button").classList = ("appear");
+    document.querySelector("#splash_button").classList = ("up_and_down");
+
+}
+
+
+/////////HER STARTER SCROLL EVENTS ////////////////////////////////
+
+
+
+//source: https://gomakethings.com/how-to-test-if-an-element-is-in-the-viewport-with-vanilla-javascript/
+
+//her siger vi at isInViewport er en funktion der kan reagere med et element
+//i dette tilfælde test
+//så tjekker bounding om vores element er indenfor viewporten
+let isInViewport = function (elem) {
+    let bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
+
+
+//her giver vi bare div'en et kortere navn
+let test = document.querySelector("#test_skrift");
+
+//her tilføjer vi en eventlistener når vi scroller
+window.addEventListener('scroll', function (event) {
+    //her siger vi at hvis vores test element er indenfor viewporten - det er det vi tjekker i isInViewport - skal elementet test_skrift begynde en animation der hedder up_and_down
+    if (isInViewport(test)) {
+        document.querySelector("#test_skrift").classList = ("up_and_down")
+    }
+}, false);
